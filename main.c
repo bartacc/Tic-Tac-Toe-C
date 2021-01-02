4015e2db92327259d09a8b59d163040c004805cf
Bartek Szczeciński <bartosz.szczecinski@protonmail.com>
2021-01-02 16:27:29 +0100

    Add object unref when the app starts

--- main.c
+++ main.c
@@ -2,5 +2,8 @@
 #include "src/app/app.h"
 
 int main(int argc, char *argv[]) {
-    return g_application_run(G_APPLICATION (app_new()), argc, argv);
+    GApplication *a = G_APPLICATION(app_new());
+    int status = g_application_run(a, argc, argv);
+    g_object_unref(a);
+    return status;
 }

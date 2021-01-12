19cc070d03e8c473a2740dfd178264033c6f48d2
Bartek Szczeciński <bartosz.szczecinski@protonmail.com>
2021-01-12 01:55:05 +0100

    Update app to non unique

--- src/app/app.c
+++ src/app/app.c
@@ -18,16 +18,11 @@ static void app_activate(GApplication *gApp) {
     gtk_window_present(GTK_WINDOW(win));
 }
 
-static void app_open(GApplication *gApp, GFile **files, int n_files, const char *hint) {
-    app_activate(gApp);
-}
-
 static void app_class_init(AppClass *class) {
     G_APPLICATION_CLASS(class)->activate = app_activate;
-    G_APPLICATION_CLASS(class)->open = app_open;
 }
 
 App *app_new(void) {
     return g_object_new(APP_TYPE, "application-id", "com.github.bartacc.tictactoe-c",
-                        "flags", G_APPLICATION_HANDLES_OPEN, NULL);
+                        "flags", G_APPLICATION_NON_UNIQUE, NULL);
 }

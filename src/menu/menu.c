6e333a3bbe9056814075f13fff75d1a2c150e781
Bartek Szczeciński <bartosz.szczecinski@protonmail.com>
2021-01-12 01:57:42 +0100

    Refactor menu init

--- src/menu/menu.c
+++ src/menu/menu.c
@@ -6,24 +6,17 @@ void menu_init(AppWindow *window, GtkGrid *menu) {
     for (GList *elem = children; elem != NULL; elem = elem->next) {
         gpointer data = elem->data;
 
-        const GtkWidget *widget = GTK_WIDGET(data);
+        GtkWidget *widget = GTK_WIDGET(data);
         const gchar *name = gtk_widget_get_name(widget);
 
         if (strcmp(name, "menu_play_as_1_button") == 0) {
-            g_signal_connect(widget, "clicked", app_window_play_as_1, window);
-            continue;
-        }
-        if (strcmp(name, "menu_play_as_2_button") == 0) {
-            g_signal_connect(widget, "clicked", app_window_play_as_2, window);
-            continue;
-        }
-        if (strcmp(name, "menu_how_to_play_button") == 0) {
-            g_signal_connect(widget, "clicked", app_window_how_to_play_show, window);
-            continue;
-        }
-        if (strcmp(name, "menu_quit_button") == 0) {
-            g_signal_connect(widget, "clicked", app_window_quit, window);
-            continue;
+            g_signal_connect(widget, "clicked", G_CALLBACK(app_window_play_as_1), window);
+        } else if (strcmp(name, "menu_play_as_2_button") == 0) {
+            g_signal_connect(widget, "clicked", G_CALLBACK(app_window_play_as_2), window);
+        } else if (strcmp(name, "menu_how_to_play_button") == 0) {
+            g_signal_connect(widget, "clicked", G_CALLBACK(app_window_how_to_play_show), window);
+        } else if (strcmp(name, "menu_quit_button") == 0) {
+            g_signal_connect(widget, "clicked", G_CALLBACK(app_window_quit), window);
         }
     }
 }
